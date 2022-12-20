03eada09365d07bc79cb76fcac696d8641a0fe3c
Tadeuvelloso <tadeuvellososilva@gmail.com>
2022-12-20 16:44:37 -0300

    feat: database

---
+++ dump.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE shortly_api;
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(90) NOT NULL UNIQUE,
+	email TEXT NOT NULL UNIQUE,
+	password VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE links (
+	id SERIAL PRIMARY KEY,
+	"userId" INTEGER NOT NULL REFERENCES "users"("id"),
+	"url" TEXT NOT NULL,
+	"shortUrl" TEXT NOT NULL,
+	"visitCount" INTEGER NOT NULL
+);
+
+CREATE TABLE sessions (
+	id SERIAL PRIMARY KEY,
+	"userId" INTEGER NOT NULL REFERENCES "users"("id"),
+	token TEXT NOT NULL
+);
+
+CREATE TABLE ranking (
+	id SERIAL PRIMARY KEY,
+	"userId" INTEGER NOT NULL REFERENCES "users"("id"),
+	"linksCount" INTEGER NOT NULL,
+	"allVisitCount" INTEGER NOT NULL
+);
